6402742d1c4898f1f75ec995ca3ba82e9f561161
daniel <danielnava901@hotmail.com>
2024-02-16 13:24:43 -0600

    commit

---
+++ src/components/JsonToRender.tsx
@@ -0,0 +1,125 @@
+import React, {useEffect} from 'react';
+import reactStringReplace from "react-string-replace";
+
+function urlify(text) {
+    return reactStringReplace(text, /(https?:\/\/\S+)/g, (match, i) => (
+        <a key={i} href={match}
+           className="underline text-blue-200 is-url"
+           target="_blank">{match}</a>
+    ));
+}
+
+function isHidden(el) {
+    let style = window.getComputedStyle(el);
+    return (style.display === 'none')
+}
+
+const SpanWrap = ({k, children, index}) => (
+    <div className="w-full flex">
+        <div className="flex">
+            <span className="mr-2">{k}</span>
+            <span className="mr-4">:</span>
+        </div>
+        {children}
+    </div>
+)
+
+const renderFinalContent = (key, val, index) => {
+    let renderElement;
+    switch (typeof val) {
+        case "string":
+            renderElement = <SpanWrap k={key} key={`${key}-${index}`}>
+                <span className="text-[#690] cursor-pointer lg:max-w-[490px]">
+                    "{urlify(val)}"<span className="text-violet-400">,</span>
+                </span>
+            </SpanWrap>
+            break;
+        case "number":
+            renderElement = <SpanWrap k={key} key={`${key}-${index}`}>
+                <span className="code-number text-[#a83295] cursor-pointer">
+                {val}<span className="text-violet-400"></span>
+                </span>
+            </SpanWrap>
+            break;
+        case "boolean":
+            renderElement = <SpanWrap k={key} key={`${key}-${index}`}>
+                {val ? "true" : "false"}<span className="text-violet-400">,</span>
+            </SpanWrap>
+            break;
+        default:
+            renderElement = <div key={`${key}-${index}`}>Otro</div>;
+            break;
+    }
+    return renderElement
+}
+
+export const JsonToRender = ({info, length = 0}) => {
+    const entries = Object.entries(info);
+
+    useEffect(() => {
+        const collapsableButton = document.querySelectorAll(".collapsable-button");
+        collapsableButton.forEach((item) => {
+           item.addEventListener("click", function(ev) {
+               let spanChild = item.querySelector("span")
+               let parentElement = ev.target.parentElement;
+               parentElement = parentElement.parentElement;
+               parentElement = parentElement.parentElement;
+               const collapsable = parentElement.querySelector(".collapsable");
+               console.log("c", collapsable, isHidden(collapsable));
+
+               if(isHidden(collapsable)) {
+                   collapsable.style.display = "flex";
+                   spanChild.innerText = '-';
+               }else {
+                   collapsable.style.display = "none";
+                   spanChild.innerText = '+';
+               }
+
+           })
+        });
+
+    }, []);
+
+    return <div className="w-full">
+        {
+            entries.map(([key, val] : [any, any], index) => {
+
+                if(typeof(val) === 'object' && Object.entries(val).length > 0) {
+                    const isArray = Array.isArray(val);
+                    const bracketOpen = isArray ? '[' : '{';
+                    const bracketClose = isArray ? ']' : '}';
+
+                    return <div className="w-full collapsable-father">
+                        <div className="flex items-center">
+                            <div className="collapsable-button">
+                                <span className="text-sm text-blue-500 mr-1
+                                h-4 w-4 rounded-full flex items-center justify-center
+                                hover:opacity-50
+                                hover:bg-blue-200
+                                cursor-pointer">-</span>
+                            </div>
+
+                            <span>{!isNaN(+key) ? '' : <span>{key}:</span>}</span>
+                            <span className="text-red-600 font-bold"> {bracketOpen}</span>
+                        </div>
+                        <div className="ml-8 collapsable">
+                            {JsonToRender({info: val, length: val.length})}
+                        </div>
+                        <div>
+                            <span className="text-red-600 font-bold">{bracketClose}</span>
+                            {(length - 1) != index ? <span className="text-violet-400">,</span> : null}
+                        </div>
+                    </div>
+                }else {
+                    if (Array.isArray(val)) {
+                        return <div className="text-red-600 font-bold">[]</div>
+                    }else if(typeof val === "object"){
+                        return <div className="text-red-600 font-bold">{}</div>
+                    }else {
+                        return renderFinalContent(key, val, index);
+                    }
+                }
+            })
+        }
+    </div>
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,157 +1,27 @@
-import * as React from "react"
+import React, {useState} from 'react';
 import type { HeadFC, PageProps } from "gatsby"
 import cvJson from '../data/cv.json';
-import {useEffect, useState} from "react";
-import reactStringReplace from 'react-string-replace';
-import {ViewOuter} from "../components/ViewOuter";
+import {JsonToRender} from "../components/JsonToRender";
 
-function urlify(text) {
-    return reactStringReplace(text, /(https?:\/\/\S+)/g, (match, i) => (
-        <a key={i} href={match}
-           className="underline text-blue-200 is-url"
-           target="_blank">{match}</a>
-    ));
-}
-
-const renderValue = (keyVal, key, index) => {
-    let renderElement = null;
-    switch (typeof keyVal) {
-        case "string":
-            renderElement = <span key={`${key}-${index}`}
-                              className="text-[#690] cursor-pointer lg:max-w-[490px]">
-                "{urlify(keyVal)}",
-            </span>
-            break;
-        case "number":
-            renderElement = <span key={`${key}-${index}`}
-                              className="code-number text-[#a83295] cursor-pointer">
-                {keyVal},
-            </span>
-            break;
-        case "boolean":
-            renderElement = <span key={`${key}-${index}`}
-                              className="code-number text-[#a83295] cursor-pointer">{
-                keyVal ? "true" : "false"
-            },</span>
-            break;
-        default:
-            renderElement = <div
-                key={`${key}-${index}`}
-                className="code-line">
-                {
-                    Array.isArray(keyVal) ? <span className="flex flex-col">
-                        <div className="code-line">
-                            <pre className="start-bracket">&#91;</pre>
-                        </div>
-                        {
-                            keyVal.map((subObj, i) => {
-                                let subKeys = Object.keys(subObj);
-                                return <div
-                                    key={`${key}-${i}`}
-                                    className="code-line pl-4 -left-8">
-                                    <div className="code-line">
-                                        <pre className="text-violet-500">&#123;</pre>
-                                    </div>
-                                    <div className="pl-4">
-                                        {
-                                            subKeys.map((sk, inx) => {
-                                                return renderValue(subObj[sk], sk, "pepe");
-                                            })
-                                        }
-                                    </div>
-                                    <div className="code-line">
-                                        <pre className="text-violet-500">&#125;,</pre>
-                                    </div>
-                                </div>
-                            })
-                        }
-                        <div className="code-line">
-                            <pre className="end-bracket">&#93;</pre>
-                        </div>
-                    </span> : (
-                    typeof keyVal === "object" ? <span>
-                            <div className="code-line">
-                                <pre className="text-violet-500">&#123;</pre>
-                            </div>
-                            {
-                                Object.keys(keyVal)
-                                    .map((sk, inx) => {
-                                    return renderValue(keyVal[sk], sk, inx);
-                                })
-                            }
-                            <div className="code-line">
-                                <pre className="text-violet-500">&#125;</pre>
-                            </div>
-                        </span> :
-                        <span>
-                            otro
-                        </span>
-                    )
-                }
-            </div>;
-            break;
-    }
-
-    return <span className="mr-4 mb-2 flex">
-        <div className="flex justify-center">
-            <span className="cursor-pointer">"{key}"</span>
-            <span className="mx-2">:</span>
-        </div>
-        {renderElement}
-    </span>
-}
-
-const IndexPage: React.FC<PageProps> = () => {
+const IndexPage : React.FC<PageProps> = () => {
     const [data, setData] = useState(cvJson);
-    const [keys, setKeys] = useState(Object.keys(data));
-    const [currentUrlViewer, setCurrentUrlViewer] = useState<string|null>(null);
-    const [showViewer, setShowViewer] = useState(true);
-    useEffect(()=> {
-        let isUrlElements = document.querySelectorAll(".is-url");
-        isUrlElements.forEach(element => {
-           element.addEventListener("mouseenter", () => {
-               let url = element.attributes.getNamedItem("href").value;
-               console.log({url});
-               setCurrentUrlViewer(url);
-               setShowViewer(true);
-           });
 
-
-        });
-
-        return () => {
-            let isUrlElements = document.querySelectorAll(".is-url");
-            isUrlElements.forEach(element => {
-                element.removeEventListener("mouseenter", () => {
-                    setCurrentUrlViewer(null);
-                })
-            });
-        }
-    }, []);
-  return (
-    <main className="
+    return <main className="
         w-[100vw]
         h-[100vh]
         text-[0.9rem]
         lg:text-md relative
         ">
-
-      <div className="w-full h-full p-3 font-mono">
-          <div className="code-line"><pre className="text-gray-200">&#123;</pre></div>
-          {
-              keys.map((key, index) => {
-                  return <div
-                      key={key}
-                      className="code-line flex pl-4 hover:bg-[rgba(131,141,155,0.3)] transition duration-500">
-                      { renderValue(data[key], key, index) }
-                  </div>
-              })
-          }
-          <div className="code-line"><pre className="text-gray-200">&#125;</pre></div>
-      </div>
-
+        <div className="w-full h-full p-3 font-mono flex flex-col">
+            <div><span className="text-red-600 font-bold">&#123;</span></div>
+            <div className="w-full ml-8">
+                <JsonToRender info={data} length={Object.entries(data).length} />
+            </div>
+            <div>
+                <div><span className="text-red-600 font-bold">&#125;</span></div>
+            </div>
+        </div>
     </main>
-  )
 }
 
 export default IndexPage
