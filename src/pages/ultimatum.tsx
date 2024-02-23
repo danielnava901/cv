c80cf669d8097931f7f28ddd65fe53de7393c698
daniel <danielnava901@hotmail.com>
2024-02-23 14:40:35 -0600

    commit

--- src/pages/ultimatum.tsx
+++ src/pages/ultimatum.tsx
@@ -1,8 +1,10 @@
 import React, {useEffect, useState} from 'react';
 
 const Ultimatum = () => {
-    const [today, setToday] = useState(new Date());
-    const [limitDate, setLimitDate] = useState(new Date('2024-6-17'));
+    const [today] = useState(new Date());
+    const [startEFDate] = useState(new Date('2024-6-16'));
+    const [endDateEF] = useState(new Date('2024-11-15'));
+
     const daysOfYear = 366; /*2024*/
     const daysArray = new Array(daysOfYear).fill(1);
 
@@ -10,8 +12,12 @@ const Ultimatum = () => {
         Math.floor((date - new Date(date.getFullYear(), 0, 0)) / 86_400_000);
 
     const todayDayOfYear = dayOfYear(today);
-    const limitDayOfYear = dayOfYear(limitDate);
+    const startEFDateDayOfYear = dayOfYear(startEFDate);
+    const endDateEFDayOfYear = dayOfYear(endDateEF);
+
     const vacation = 10;
+    const secondVacation = 5;
+    const livingInNz = 15;
 
     return <div className="
         w-[100vw]
@@ -20,7 +26,21 @@ const Ultimatum = () => {
         lg:text-md relative
         fira-mono-medium"
     style={{zIndex: 9}} >
+        <div className="w-full flex p-4 justify-between">
+            <div className="p-2 mr-4 flex flex-col">
+                <div className="text-lg font-bold text-white">Hoy es:</div>
+                <div className="text-xs font-bold text-white">{today.toLocaleDateString()}</div>
+            </div>
+            <div className="p-2 mr-4 flex flex-col">
+                <div className="text-lg font-bold text-white">Dias restantes</div>
+                <div className="text-xs font-bold text-white">{startEFDateDayOfYear - todayDayOfYear}</div>
+            </div>
+            <div className="p-2 mr-4 flex flex-col">
+                <div className="text-lg font-bold text-white">Dias en NZ</div>
+                <div className="text-xs font-bold text-white">{endDateEFDayOfYear + livingInNz - startEFDateDayOfYear}</div>
+            </div>
 
+        </div>
         <div className="w-full flex flex-wrap">
             {
                 daysArray.map((day, index) => {
@@ -31,14 +51,24 @@ const Ultimatum = () => {
                     rounded-full
                     flex justify-center items-center
                     text-white
-                    ${ realIndex <= todayDayOfYear ? 'bg-yellow-600' :
-                        realIndex < limitDayOfYear ? (
-                            realIndex > (limitDayOfYear - vacation) && realIndex < limitDayOfYear 
-                               ? 'bg-rose-500' : 'bg-green-600'
-                       ): ''
-                    }
-                    ${(index + 1) === dayOfYear(today) ? 'twinkle' : ''}
-                    ${(index + 1) === dayOfYear(limitDate) ? 'twinkle bg-rose-800' : ''}
+                    
+                    Dias_pasados
+                    ${realIndex <= todayDayOfYear ? 'bg-yellow-600' : ''}
+                    Hoy
+                    ${realIndex === todayDayOfYear ? 'twinkle' : ''}
+                    Dias_antes_de_iniciar_EF
+                    ${realIndex < startEFDateDayOfYear ? 'bg-green-600' : '' }
+                    Dias_de_vacaciones_antes_de_iniciar_EF
+                    ${realIndex > (startEFDateDayOfYear - vacation) && realIndex < startEFDateDayOfYear ? 'bg-rose-500' : ''}
+                    Fecha_de_inicio_EF_(17_Jun_24)
+                    ${realIndex === startEFDateDayOfYear ? 'twinkle' : ''}
+                    Estancia_En_NZ
+                    ${realIndex >= startEFDateDayOfYear && realIndex <= endDateEFDayOfYear ? 'bg-blue-600' : ''}
+                    Tiempo_en_NZ_despues_de_EF
+                    ${realIndex > endDateEFDayOfYear && realIndex <= (endDateEFDayOfYear + livingInNz) ? 'bg-blue-900' : ''}
+                    Timpo_vacaciones_2
+                    ${realIndex > (endDateEFDayOfYear + livingInNz) && 
+                        realIndex <= (endDateEFDayOfYear + livingInNz + secondVacation) ? 'bg-rose-900' : ''}
                    `}
 
                    >
